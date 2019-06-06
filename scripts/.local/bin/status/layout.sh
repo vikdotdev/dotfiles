f75ba7bd074e8794e7e28f2fd47a5c558f97f54c
vikdotdev <viktorhbk@gmail.com>
2019-06-06 19:38:46 +0300

    moved scripts to more appropriate location

--- scripts/.local/bin/status/layout.sh
+++ scripts/.local/bin/status/layout.sh
@@ -2,6 +2,6 @@
 langcode=$(xset -q | grep -A 0 'LED' | cut -c59-67)
 
 case $langcode in
-  00001000) echo "ua" ;;
-  00000000) echo "eng" ;;
+  00001000) echo "ua" && exit 0 ;;
+  00000000) echo "eng" && exit 0 ;;
 esac
--- scripts/.local/bin/tools/full_backup.sh
+++ scripts/.local/bin/tools/full_backup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# this scripts needs overhaul: fix incorrect path, implement prompts onlydry run
 sudo rsync -aAXvP --delete --exclude=/swapfile --exclude=/home/vik/dl/* --exclude=/home/vik/.cache/*  --exclude=/home/vik/repos/* --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/mnt/* --exclude=/media/* --exclude=/lost+found / "$1"
