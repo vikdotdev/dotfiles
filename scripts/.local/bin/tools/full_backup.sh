f25a13d451203af56c7b4cc428a9a134aec582b2
vikdotdev <viktorhbk@gmail.com>
2019-07-22 14:19:11 +0300

    finished base version on the backup script

--- scripts/.local/bin/tools/full_backup.sh
+++ scripts/.local/bin/tools/full_backup.sh
@@ -1,24 +1,57 @@
 #!/bin/bash
+
 MOUNT_POINT="/mnt/backup"
+BACKUP_CONTENTS="/home/vik/" # note the trailing space here
 LS=$(lsblk -l | awk '{print $1,$4,$7}' | grep 'sd[a-z][0-9]')
 CORRECT_DEVICES=$(lsblk -l | awk '{print $1}' | grep 'sd[a-z][0-9]')
 DEVICE=
 
-echo "$LS" | grep "$MOUNT_POINT" &> /dev/null
-if [ $? == 0 ]; then
-  echo "There's already something mounted onto $MOUNT_POINT. Run lsblk command, unmount it manually and try again."
-  exit 1
-else
+function list_devices {
   echo "DEV SIZE MOUNTED"
   echo "$LS"
-  echo -e -n "\nChoose a drive name from list to mount onto "$MOUNT_POINT": "
+}
+
+function read_device_name {
+  echo -e -n "\nChoose a device name to mount onto "$MOUNT_POINT": "
   read DEVICE
   echo -e "$CORRECT_DEVICES" | grep -w "$DEVICE" &> /dev/null
+}
+
+function create_datafiles {
+  echo "Creating datafiles..."
+}
+
+function sync {
+  echo -e "\nDo not change backup files during transfer.\n"
+  sleep 1
+  echo Backing up "$BACKUP_CONTENTS" to "$MOUNT_POINT"
+  sudo rsync -aAXvP --delete "$BACKUP_CONTENTS" "$MOUNT_POINT" # what -> where
+}
+
+function mount_device {
+  sudo mount $(echo "/dev/$DEVICE") "$MOUNT_POINT"
+}
+
+function main {
+  echo "$LS" | grep "$MOUNT_POINT" &> /dev/null
   if [ $? == 0 ]; then
-    echo Correct
+    echo -e "\nThere's already something mounted onto $MOUNT_POINT. Unmount it manually and try again.\n"
+    list_devices
+    exit 1
   else
-    echo "$DEVICE" device is incorrect. Aborting.
-    exit 2
+    list_devices
+    read_device_name
+    if [ $? == 0 ]; then
+      create_datafiles
+      mount_device
+      sync
+    else
+      echo "$DEVICE" is not a correct device. Aborting.
+      exit 1
+    fi
   fi
-fi
-# sudo rsync --dry-run -aAXvP --delete --exclude=/swapfile --exclude=/home/vik/dl/* --exclude=/home/vik/.cache/*  --exclude=/home/vik/repos/* --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/mnt/* --exclude=/media/* --exclude=/lost+found / /mnt/usb1/
+}
+
+main
+
+# sudo rsync --dry-run -aAXvP --delete --exclude=/swapfile --exclude=/home/vik/dl/* --exclude=/home/vik/.cache/*  --exclude=/home/vik/repos/* --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/* --exclude=/run/* --exclude=/mnt/* --exclude=/media/* --exclude=/lost+found / "$MOUNT_POINT"
