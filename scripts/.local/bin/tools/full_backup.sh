1c397c5111a066783d7d27311273738f4600fc46
vikdotdev <viktorhbk@gmail.com>
2019-07-25 16:46:24 +0300

    fixed backup script paths, excludes. typo in gitignore

--- scripts/.local/bin/tools/full_backup.sh
+++ scripts/.local/bin/tools/full_backup.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-MOUNT_POINT="/mnt/backup"
-BACKUP_CONTENTS="/home/vik/" # note the trailing space here
+MOUNT_POINT="/mnt/backup/" # note the trailing space here
+BACKUP_CONTENTS="/home/vik"
 LS=$(lsblk -l | awk '{print $1,$4,$7}' | grep 'sd[b-z][0-9]')
 CORRECT_DEVICES=$(lsblk -l | awk '{print $1}' | grep 'sd[b-z][0-9]')
 DEVICE=
 
 function list_devices {
   echo "DEV  SIZE  MOUNTED"
-  local list=$(lsblk -l | awk '{print $1,$4,$7}' | grep 'sd[b-z][0-9]')
-  echo "$list"
+  local LIST=$(lsblk -l | awk '{print $1,$4,$7}' | grep 'sd[b-z][0-9]')
+  echo "$LIST"
 }
 
 function read_device_name {
-  # echo -e -n "\nChoose a device name to mount onto "$MOUNT_POINT": "
-  # read DEVICE
-  DEVICE=$(echo "$CORRECT_DEVICES" | rofi_dmenu.sh "Choose a device name to mount onto $MOUNT_POINT")
+  local PROMPT="Choose a device name to mount onto $MOUNT_POINT"
+  if [ -x "$(command -v rofi)" ]; then
+    DEVICE=$(echo "$CORRECT_DEVICES" | rofi_dmenu.sh "$PROMPT")
+  else
+    echo -e -n "\n$PROMPT: "
+    read DEVICE
+  fi
   echo -e "$CORRECT_DEVICES" | grep -w "$DEVICE" &> /dev/null
 }
 
@@ -27,16 +31,17 @@ function sync {
   echo -e "\nDo not change backup files during transfer.\n"
   sleep 1
   echo Backing up "$BACKUP_CONTENTS" to "$MOUNT_POINT"
-  sudo rsync -aAXvP --delete "$BACKUP_CONTENTS" "$MOUNT_POINT" # what -> where
+  sudo rsync -aAXvP --delete --exclude=/vik/repos/*/node_modules --exclude=/vik/.npm --exclude=/vik/.config/chromium --exclude=/vik/dl/* --exclude=/vik/temp/* --exclude=/vik/.cache/* "$BACKUP_CONTENTS" "$MOUNT_POINT" # what -> where
 }
 
 function mount_device {
+  echo "Mounting $DEVICE"
   sudo mount $(echo "/dev/$DEVICE") "$MOUNT_POINT"
 }
 
 function main {
   LS=$(list_devices)
-  echo -e "$LS" | grep "$MOUNT_POINT" &> /dev/null
+  echo -e "$LS" | grep "${MOUNT_POINT::-1}" &> /dev/null
   if [ $? == 0 ]; then
     list_devices
     echo -e -n "\nThere's already something mounted onto $MOUNT_POINT. Unmount? [y/N]: "
