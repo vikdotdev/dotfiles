02bafce38a40b3074d4ca0477bd5d1033d4e7071
Viktor Habchak <vikdotdev@gmail.com>
2020-08-30 17:25:04 +0300

    add git cred-store script, fix script modes

---
+++ bin/bin/s-setup-git-store.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt-get update && sudo apt-get install libsecret-1-0 libsecret-1-dev
+cd /usr/share/doc/git/contrib/credential/libsecret
+sudo make
+git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
