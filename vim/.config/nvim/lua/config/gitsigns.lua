6fb29e7ac58029bf0627fbf5bf27e9c55d7b4c95
Viktor Habchak <vikdotdev@gmail.com>
2021-09-20 22:59:59 +0300

    update gitsigns

--- vim/.config/nvim/lua/config/gitsigns.lua
+++ vim/.config/nvim/lua/config/gitsigns.lua
@@ -21,5 +21,7 @@ require('gitsigns').setup({
   update_debounce = 100,
   status_formatter = nil, -- Use default
   word_diff = false,
-  use_internal_diff = true,  -- If luajit is present
+  diff_opts = {
+    internal = true
+  }
 })
