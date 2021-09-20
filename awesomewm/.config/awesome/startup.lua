bf170244fa5b973091ea43220c3329b87971a1b5
Viktor Habchak <vikdotdev@gmail.com>
2021-09-20 23:27:34 +0300

    add random wallpaper script

--- awesomewm/.config/awesome/startup.lua
+++ awesomewm/.config/awesome/startup.lua
@@ -2,7 +2,7 @@ local awful = require("awful")
 
 commands = {
   'xrandr --output DP-2 --mode 3440x1440 --rate 144',
-  'feh --bg-fill /home/vik/Pictures/wallpapers/961101.jpg',
+  'random-wallpaper',
   'killall compton && compton --vsync opengl',
   'xset r rate 250 45',
   'xinput set-prop "$(xinput list --name-only | grep -i touchpad)" "libinput Tapping Enabled" 1'
