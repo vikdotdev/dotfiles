dce0eb40548c75b86cabd7df133ef8bdb2254e0d
Viktor Habchak <vikdotdev@gmail.com>
2021-10-11 21:49:25 +0300

    awesomewm add transparency; fix compton

--- awesomewm/.config/awesome/startup.lua
+++ awesomewm/.config/awesome/startup.lua
@@ -3,7 +3,7 @@ local awful = require("awful")
 commands = {
   'xrandr --output DP-2 --mode 3440x1440 --rate 144',
   'random-wallpaper',
-  'killall compton && compton --vsync opengl',
+  'killall compton || compton --vsync opengl',
   'xset r rate 250 45',
   'xinput set-prop "$(xinput list --name-only | grep -i touchpad)" "libinput Tapping Enabled" 1'
 }
