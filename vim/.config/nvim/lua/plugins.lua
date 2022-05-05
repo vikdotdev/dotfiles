1b3d4a20a712944dba4a276b0b29968625156650
Viktor Habchak <vikdotdev@gmail.com>
2022-05-05 12:02:30 +0300

    upgrade config to neovim 0.7

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -1,11 +1,6 @@
 return require('packer').startup(function(use)
   use({'wbthomason/packer.nvim'})
-  use({
-    'nvim-telescope/telescope.nvim',
-    requires = {'nvim-lua/plenary.nvim'},
-    -- requires nvim ^0.7
-    commit = 'd88094fbfd84b297178252230f6faf0e7d2f7650'
-  })
+  use({'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim'}})
   use({'neovim/nvim-lspconfig'})
   use({'hrsh7th/cmp-nvim-lsp'})
   use({'hrsh7th/cmp-buffer'})
@@ -14,22 +9,15 @@ return require('packer').startup(function(use)
   use({'hrsh7th/nvim-cmp'})
   use({'folke/which-key.nvim'})
   use({'norcalli/nvim-colorizer.lua'})
-  use({
-    'numToStr/Comment.nvim',
-    config = function() require('Comment').setup() end,
-    -- requires nvim ^0.7
-    tag = 'v0.6'
-  })
+  use({ 'numToStr/Comment.nvim', config = function() require('Comment').setup() end })
   use({'JoosepAlviste/nvim-ts-context-commentstring'})
   use({'ojroques/nvim-bufdel'})
   use({'vikdotdev/bufnav.nvim'})
-  use({'drewtempelmeyer/palenight.vim'})
+  use({'projekt0n/github-nvim-theme'})
   use({'nvim-treesitter/nvim-treesitter',
     ensure_installed = 'maintained',
     run = {':TSUpdate'},
     branch = 'master',
-    -- requires nvim ^0.7
-    commit = '8a1acc00d2a768985a79358d1a6caa9f08a0eeea'
   })
   use({'romgrk/nvim-treesitter-context'})
   use({'mhinz/vim-startify'})
@@ -38,10 +26,7 @@ return require('packer').startup(function(use)
     requires = { 'nvim-lua/plenary.nvim' },
     config = function() require('gitsigns').setup() end
   })
-  use({
-    'kyazdani42/nvim-tree.lua',
-    commit = 'fd2332a33f0fc7c8545cd1c358ad032876318a43'
-  })
+  use({'kyazdani42/nvim-tree.lua'})
   use({'L3MON4D3/LuaSnip'})
   use({'kmonad/kmonad-vim'})
   use({'deris/vim-shot-f'})
