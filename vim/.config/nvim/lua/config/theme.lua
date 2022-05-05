f62136e00959551809d8bf3e3924ec5be224bd42
Viktor Habchak <vikdotdev@gmail.com>
2022-05-05 14:19:27 +0300

    add alacritty config builder script

--- vim/.config/nvim/lua/config/theme.lua
+++ vim/.config/nvim/lua/config/theme.lua
@@ -1,3 +1,15 @@
-vim.g.palenight_color_overrides = {
-  black = { gui = 'NONE', cterm = "NONE", cterm16 = 'NONE' }
-}
+require('github-theme').setup({
+  dark_float = true,
+  hide_inactive_statusline = false,
+  sidebars = {'packer', 'telescope'},
+  theme_style = 'dark',
+  transparent = true,
+  comment_style = "NONE",
+  keyword_style = "NONE"
+})
+
+-- TODO match colors or gitsigns to the theme?
+-- nvim tree color when picking a window A B
+-- depending on the theme, tmux blink should change color
+-- work on dark gtk themes
+-- different color for classes and constants
