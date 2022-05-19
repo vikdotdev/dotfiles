35a4fbf45eabdba2c0aa47f7052ffc627770a6d9
Viktor Habchak <vikdotdev@gmail.com>
2022-05-19 19:48:01 +0300

    fix rvm scripts

--- plans/ruby/builder.rb
+++ plans/ruby/builder.rb
@@ -1,5 +1,7 @@
 %w[after_use_ensure_gems_installed after_use_print_ruby_version].each do |file|
-  ensure_write_file(home_file(".rvm/hooks/#{file}"), read_file(file))
+  absolute_file_path = home_file(".rvm/hooks/#{file}")
+  ensure_write_file(absolute_file_path, read_file(file))
+  chmod('+x', absolute_file_path)
 end
 
 ensure_write_file(home_file('.pryrc'), read_file('.pryrc'))
