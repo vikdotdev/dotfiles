0313977ac51e49a212dd1f5dd3ecaf43cef9fdf7
Viktor Habchak <vikdotdev@gmail.com>
2022-05-22 18:43:48 +0300

    remove imwheel; TODO add display env

--- plans/imwheel/builder.rb
+++
@@ -1,3 +0,0 @@
-ensure_write_file(home_file('.imwheelrc'), read_file('.imwheelrc'))
-ensure_write_file(home_file('.config/systemd/user/imwheel.service'), read_file('imwheel.service'))
-ensure_service_running('imwheel.service')
