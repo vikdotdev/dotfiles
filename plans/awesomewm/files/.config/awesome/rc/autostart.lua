3a9d4f82ec8ed679f9fbcce90fe6dcd6b0fe88be
Viktor Habchak <vikdotdev@gmail.com>
2022-06-25 12:46:38 +0300

    remove gnome, add awesomewm autostart back

---
+++ plans/awesomewm/files/.config/awesome/rc/autostart.lua
@@ -0,0 +1,4 @@
+local awful = require('awful')
+
+awful.spawn.easy_async_with_shell('$HOME/bin/sys-autostart')
+
--- plans/gnome/builder.rb
+++
@@ -1,4 +0,0 @@
-ensure_write_file(
-  home_file('.config/autostart/autostart.desktop'),
-  render_template('autostart.desktop')
-)
