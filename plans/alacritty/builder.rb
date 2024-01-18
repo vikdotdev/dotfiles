28a8eacc03c3a73d4d7ad9b39c99955e7a1f6835
Viktor Habchak <vikdotdev@gmail.com>
2024-01-18 18:26:19 +0100

    Deprecate alacritty yml config

--- plans/alacritty/builder.rb
+++ plans/alacritty/builder.rb
@@ -1,3 +1,7 @@
-destination = home_file '.config/alacritty/alacritty.yml'
-contents = render_template 'alacritty.yml'
+# TODO: DEPRECATED
+# destination = home_file '.config/alacritty/alacritty.yml'
+# contents = render_template 'alacritty.yml'
+
+destination = home_file '.config/alacritty/alacritty.toml'
+contents = render_template 'alacritty.toml'
 ensure_write_file(destination, contents)
