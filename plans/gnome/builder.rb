ef4b97345514ddaa0782883105a72643b4b0e67d
Viktor Habchak <vikdotdev@gmail.com>
2024-06-01 18:45:35 +0200

    install gnome extension

--- plans/gnome/builder.rb
+++ plans/gnome/builder.rb
@@ -1,8 +1,6 @@
-# install extension in ansible?
 system("dconf load / < #{file_path('dconf.ini')}")
 
 ensure_write_file(home_file('.config/gtk-3.0/gtk.css'), read_file('gtk3.css'))
 ensure_write_file(home_file('.config/gtk-3.0/settings.ini'), read_file('settings3.ini'))
 
 ensure_write_file(home_file('.config/gtk-4.0/gtk.css'), read_file('gtk4.css'))
-# add extension shortcuts
