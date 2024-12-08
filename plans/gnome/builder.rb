fd0f8169c4033e577779517912a899851a5203b3
Viktor Habchak <vikdotdev@gmail.com>
2024-12-08 10:53:18 +0100

    android and a bunch of stuff

--- plans/gnome/builder.rb
+++ plans/gnome/builder.rb
@@ -6,3 +6,9 @@ ensure_write_file(home_file('.config/gtk-3.0/gtk.css'), read_file('gtk3.css'))
 ensure_write_file(home_file('.config/gtk-3.0/settings.ini'), read_file('settings3.ini'))
 
 ensure_write_file(home_file('.config/gtk-4.0/gtk.css'), read_file('gtk4.css'))
+
+absolute_file_path = home_file(".local/share/applications/windows.desktop")
+ensure_write_file(absolute_file_path, read_file("windows.desktop"))
+
+absolute_file_path = home_file(".local/share/icons/windows.png")
+ensure_write_file(absolute_file_path, read_file("windows.png"))
