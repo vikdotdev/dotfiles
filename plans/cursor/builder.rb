8b76def978b4888edd91f2d4098d69c732b33889
Viktor Habchak <vikdotdev@gmail.com>
2025-08-05 20:34:00 +0200

    remove unused config

--- plans/cursor/builder.rb
+++
@@ -1,5 +0,0 @@
-absolute_file_path = home_file(".local/share/applications/cursor-editor.desktop")
-ensure_write_file(absolute_file_path, read_file("cursor-editor.desktop"))
-
-absolute_file_path = home_file(".local/share/icons/cursor-editor.png")
-ensure_write_file(absolute_file_path, read_file("cursor-editor.png"))
